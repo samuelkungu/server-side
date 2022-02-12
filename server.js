9f05661cec214060473b6dbb970ba5b1debe2294
Samuel <kungusamuel64@gmail.com>
2022-02-12 16:19:33 +0300

    Recent Updates

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const mssql = require('mssql')
+require('dotenv').config()
 const app = express()
+
+
+//connecting to database
 
 app.get('/', function (req, res) {
   res.send("This Web Application runs on port 3000")
 })
 
+app.get('/student', function(req, res){
+    res.send(" This is a list of jitu interns")
+})
+
+app.get('/student/:id', function(req, res){
+  const id = req.params.id
+  res.send(" This is a jitu interns with an ID of" + id)
+})
+
+const port = process.env.PORT
 app.listen(3000) 
     
