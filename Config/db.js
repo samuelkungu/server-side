83f2568a142cbe499eec29ec24afab5794ae0fe6
Samuel <kungusamuel64@gmail.com>
2022-02-15 22:31:59 +0300

    Recent Updates

--- Config/db.js
+++ Config/db.js
@@ -1,26 +1,27 @@
-// const mssql = required('mssql')
-//connect to db
-let config = {
-    server:'127.0.0.1',
-    user :'sa',
-    database:'Projects',
-    password :'1234',
+const mssql = require('mssql')
+const dotenv = require("dotenv")
+dotenv.config()
 
+let config = {
+    server: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
 
     options:{
         encrypt: false,
-        trustedconnection: true,
-        enableArithAbort : true, 
-        instancename :'MSSQL_SERVER'
     },
-
-    pool: {
-        max: 10,
-        min: 0,
+    pool:{
+        max:10,
+        min:0,
         idleTimeoutMillis: 30000
-    },
-
-    // port : 1031
+    }
 }
-
-module.exports = config; 
+mssql.connect(config).then(pool =>{
+    if(pool.connecting){
+        console.log('connecting to the database');
+    }
+    if(pool.connected){
+        console.log("connected");
+    }
+}).catch(e=> console.log(e))
--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,81 +1,77 @@
-const config = require('../Config/db');
-const mssql = require('mssql');
+const mssql = require('mssql')
+const config = require('../config/db')
 
-
-async function getUsers(req,res) {
-    try {
+async function getUsers (req,res){
+    try{
         await mssql.connect(config)
         const result = await (await mssql.query("SELECT * FROM users")).recordset
-        res.send(result)
-        console.log(result)
-
-    } catch (err) {
-        // ... error checks
-        console.log(err)
-    }
-}
-
-async function getUser(req, res) {
-    
-    const id = req.params.id
-    try {
-        let pool = await mssql.connect(config);
-        let result1 = await pool.request()
-            .query('SELECT * from users where id = ${id}');
-        
-            res.json(result1.recordset)
-
-    }
-    catch (error) {
+        res.json(result)
+    } catch (err){
         console.log(err);
     }
 }
+async function getUser (req,res){
+    const id = req.params.id
+    try{
+        let pool = await mssql.connect(config)
+        let result1 = await pool.request()
+        .query(`select * from users where id = ${id}`)
+        res.json(result1.recordset)
 
-
-async function addUser(res, req) {
-
-    const {firstname,lastname, email, projectname} = req.body
-    const id = req.params.id;
-    try {
-        let pool = await mssql.connect(config);
+    } catch (err){
+        console.log(err);
+    }
+}
+async function addUser (req,res){
+    const{firstname, secondname, email, project, password} = req.body
+    try{
+        let pool = await mssql.connect(config)
         await pool.request()
-            .input('id', mssql.Int, id)
-            .query( 'INSERT INTO users(firtsname,lastname,email,projectname) VALUES (${firstname},${lastname},${email},${projectname}')
-        
-            res.json('User Added Successfully')
-        
-    }
-    catch (err) {
+        .query(`INSERT INTO users(firstname, secondname, email, project, password)
+            VALUES('${firstname}','${secondname}','${email}','${project}','${password}')`)
+        res.json("user added successfully")
+
+    } catch (err){
         console.log(err);
     }
-
 }
-
-async function deleteUser(req, res) {
-    
+async function updateUser (req,res){
+    const{firstname, secondname, email, project, password} = req.body
     const id = req.params.id
-    try {
-        let pool = await mssql.connect(config);
-        let result1 = await pool.request()
-            .query('DELETE from users where id = ${id}');
-        
-            res.json('User Deleted Successfully')
+    try{
+        let pool = await mssql.connect(config)
+        await pool.request()
+        .input('id', mssql.Int, id)
+        .input('firstname', mssql.VarChar, firstname)
+        .input('secondname', mssql.VarChar, secondname)
+        .input('email', mssql.VarChar, email)
+        .input('project', mssql.Text, project)
+        .input('password', mssql.VarChar, password)
+        .query('UPDATE users SET firstname = @firstname, secondname = @secondname, email = @email, project = @project, password = @password WHERE id = @id')
 
-    }
-    catch (error) {
+        res.json("user added successfully")
+
+    } catch (err){
         console.log(err);
     }
 }
+async function deleteUser (req,res){
+    const id = req.params.id
+    try{
+        let pool = await mssql.connect(config)
+        let result1 = await pool.request()
+        .query(`DELETE from users where id = ${id}`)
+        res.json("User deleted successfully")
 
-
-
-
-
+    } catch (err){
+        console.log(err);
+    }
+}
 
 module.exports = {
-    getUsers: getUsers,
-    getUser : getUser,
-    addUser : addUser,
-    // updateUser : updateUser,
-    deleteUser: deleteUser
+    getUsers,
+    getUser,
+    addUser,
+    updateUser,
+    deleteUser
 }
