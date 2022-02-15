9e00a3c5268eb07e16272ff2de059e6b3f79a0a8
Samuel <kungusamuel64@gmail.com>
2022-02-15 22:33:10 +0300

    Recent Updates

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('../Controllers/userController')
+const userController = require('../controllers/userController')
 
-router.get("/", userController.getUsers)
-router.get("/id", userController.getUser)
-router.post("/", userController.addUser)
+router.get('/',userController.getUsers)
+router.get('/:id',userController.getUser)
+router.post('/',userController.addUser)
+router.put('/:id',userController.updateUser)
+router.delete('/:id',userController.deleteUser)
 
-module.exports = router;
+module.exports = router
